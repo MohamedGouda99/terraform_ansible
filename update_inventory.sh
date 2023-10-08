ca4befb6fe15dcce40119d325a3ac6d881c265bd
Mohamed Ashraf Gouda <ma1582830@gmail.com>
2023-10-08 13:01:05 +0300

    88

--- update_inventory.sh
+++ update_inventory.sh
@@ -4,7 +4,7 @@ Get_EC2_IP() {
 
 Update_Inventory() {
     echo "It's Updating Ansible inventory"
-    inventory_file="./ansible/hosts"
+    inventory_file="/ansible/hosts"
 
     # Replace the IP address of the target host in the inventory file
     echo -e "${EC2_IP} ansible_ssh_private_key_file=tim.pem ansible_user=ec2-user" > "${inventory_file}"

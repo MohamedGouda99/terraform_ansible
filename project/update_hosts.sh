59cbb018a2acde2e83f69652aae94b77b9f1bba6
Mohamed Ashraf Gouda <ma1582830@gmail.com>
2023-10-08 20:50:41 +0300

    modifying pipeline

--- project/update_hosts.sh
+++ project/update_hosts.sh
@@ -1,10 +1,12 @@
+
 Get_EC2_IP() {
+    
     EC2_IP=$(terraform output server-ip)
 }
 
 Update_Inventory() {
     echo "It's Updating Ansible inventory"
-    inventory_file="/ansible/hosts"
+    inventory_file="../ansible/hosts"
 
     # Replace the IP address of the target host in the inventory file
     echo -e "${EC2_IP} ansible_ssh_private_key_file=tim.pem ansible_user=ec2-user" > "${inventory_file}"
